0857febd31fc75426c3fd9ca0d05948c8b2a927a
Sumon Chhetry <53363420+sumonchhetry@users.noreply.github.com>
2021-05-19 12:28:24 +0530

    Add files via upload
    #Day-1
    This is simple program which helps you to choose your band name with the help of two simple questions

---
+++ band_name_generator.py
@@ -0,0 +1,5 @@
+print("Welcome to the Band Name Generator.")
+city = input("What's the city you grow up in ? \n")
+pet = input("What's the name of your pet ? \n")
+
+print("Your Band name could be " + city + "'s " + pet)
