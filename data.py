1d02abf01faa338464ee3400d9b4cbe78f891dcb
Sumon Chhetry <53363420+sumonchhetry@users.noreply.github.com>
2021-06-04 02:38:07 +0530

    Add files via upload
    # A quiz game with OOP python

---
+++ data.py
@@ -0,0 +1,391 @@
+question_data = [
+    {
+        "category": "Entertainment: Television",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "AMC&#039;s &quot;The Walking Dead&quot;, Rick, Carl, Daryl, Morgan, Carol and Maggie were introduced to us in Season 1.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "category": "Science & Nature",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "The Neanderthals were a direct ancestor of modern humans.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "category": "Science: Computers",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "The common software-programming acronym &quot;I18N&quot; comes from the term &quot;Interlocalization&quot;.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "category": "History",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "The two atomic bombs dropped on Japan by the United States in August 1945 were named &#039;Little Man&#039; and &#039;Fat Boy&#039;.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]},
+    {
+        "category": "Entertainment: Music",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "The cover of The Beatles album &quot;Abbey Road&quot; featured a Volkswagen Beetle in the background.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "category": "Science & Nature",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "The planet Mars has two moons orbiting it.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "category": "General Knowledge",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "The vapor produced by e-cigarettes is actually water.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "category": "Entertainment: Music",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "Norwegian producer Kygo released a remix of the song &quot;Sexual Healing&quot; by Marvin Gaye.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "category": "Geography",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "Israel is 7 hours ahead of New York.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "category": "History",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "Sargon II, a king of the Neo-Assyrian Empire, was a direct descendant of Sargon of Akkad.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "category": "Geography",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "The Southeast Asian island of Borneo is politically divided among 3 countries.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "category": "History",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "Brezhnev was the 5th leader of the USSR.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "category": "Entertainment: Video Games",
+        "type": "boolean", "difficulty": "medium",
+        "question": "&quot;Metal Gear Solid 3: Snake Eater&quot; takes place on Shadow Moses Island.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "category": "General Knowledge",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "An eggplant is a vegetable.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]},
+    {
+        "category": "Entertainment: Video Games",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "Metal Gear Solid V: The Phantom Pain runs on the Fox Engine.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]},
+    {
+        "category": "General Knowledge",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "Cucumbers are usually more than 90% water.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "category": "Sports",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "Tennis was once known as Racquetball.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+     },
+    {
+        "category": "General Knowledge",
+        "type": "boolean", "difficulty": "medium",
+        "question": "The average woman is 5 inches \/ 13 centimeters shorter than the average man.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]},
+    {
+        "category": "Art",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "Pablo Picasso is one of the founding fathers of &quot;Cubism.&quot;",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "category": "History",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "If you grab the bladed end of a longsword in a specific way, you will not cut yourself.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "category": "General Knowledge",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "The term &quot;Spam&quot; came before the food product &quot;Spam&quot;.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]},
+    {
+        "category": "Geography",
+        "type": "boolean", "difficulty": "medium",
+        "question": "There exists an island named &quot;Java&quot;.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "category": "General Knowledge",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "The French word to travel is &quot;Travail&quot;",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]},
+    {
+        "category": "Entertainment: Video Games",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "TF2: The Heavy&#039;s voice actor, Gary Schwartz, voices the Demoman as well ",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]},
+    {
+        "category": "Entertainment: Comics",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "In the webcomic Homestuck, the first character introduced is Dave Strider.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "category": "History",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "Abraham Lincoln was the first U.S. President to be born outside the borders of the thirteen original states. ",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "category": "Animals",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "The Platypus is a mammal.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "category": "Entertainment: Music",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "&quot;Twenty One Pilots&quot; made the song &quot;The Motion&quot; featuring Sampha.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "category": "Mythology",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "In Greek mythology, Hera is the goddess of harvest.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "category": "Science: Computers",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "It&#039;s not possible to format a write-protected DVD-R Hard Disk.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "category": "Entertainment: Television",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "Klingons once had a period of Democracy in their history, they referred to it as the &quot;Dark Times&quot;.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "category": "Entertainment: Video Games",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "In &quot;Super Mario World&quot;, the rhino mini-boss, Reznor, is named after the lead singer of the band &quot;Nine Inch Nails&quot;.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "category": "Sports",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "Wilt Chamberlain scored his infamous 100-point-game against the New York Knicks in 1962.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "category": "Entertainment: Video Games",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "The ADAM collecters in the Bioshock series are known as Little Sisters.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "category": "General Knowledge",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "&quot;Santa Claus&quot; is a variety of melon.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "category": "Animals",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "Finnish Lapphund dogs were used for herding reindeer.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "category": "Entertainment: Television",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "An episode of &quot;The Simpsons&quot; is dedicated to Moe Szyslak&#039;s bar rag.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "category": "Entertainment: Music",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "Dave Grohl recorded the Foo Fighters&#039; debut, &quot;Foo Fighters,&quot; by himself.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "category": "Entertainment: Cartoon & Animations",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "Donald Duck played the role of Bob Cratchit in Disney&#039;s 1983 adaptation of A Christmas Carol.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "category": "General Knowledge",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "Popcorn was invented in 1871 by Frederick W. Rueckheim in the USA where he sold the snack on the streets of Chicago.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "category": "Art",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "Venus of Willendorf is one of the earliest works of art, depicting the planets Mars and Venus embrace in the heavens at night.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "category": "Science & Nature",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "The most frequent subconscious activity repeated by the human body is blinking.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "category": "Politics",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "Jeb Bush was elected as Governor of Florida in 2002, starting his political career.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "category": "General Knowledge",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "The commercial UK channel ITV stands for &quot;International Television&quot;.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]},
+    {
+        "category": "General Knowledge",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "The pickled gherkin was first added to hamburgers because a US health law required all fast-food to include a source of Vitamin C.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "category": "Sports",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "ATP tennis hosted several tournaments on carpet court before being replaced to reduce injuries.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "category": "Entertainment: Video Games",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "During pre-production of &quot;Super Mario Bros.&quot;, Mario originally was meant to shoot bullets.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "category": "Entertainment: Video Games",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "In the game &quot;Until Dawn&quot; Emily is the only playable character who can be killed by another playable character directly.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "category": "History",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "Franz Joseph I was the last emperor of Austria.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "category": "Entertainment: Video Games",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "In the Resident Evil series, Leon S. Kennedy is a member of STARS.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    }
+]
--- main.py
+++ main.py
@@ -1,23 +1,30 @@
-from menu import Menu, MenuItem
-from coffee_maker import CoffeeMaker
-from money_machine import MoneyMachine
+from question_model import Question
+from data import question_data
+from quiz_brain import QuizBrain
 
-money_machine = MoneyMachine()
-coffee_maker = CoffeeMaker()
-menu = Menu()
-is_on = True
 
-while is_on:
-    options = menu.get_items()
-    choice = input(f"What would you like ? ({options}): ")
-    if choice == "off":
-        is_on = False
-    elif choice == "report":
-        coffee_maker.report()
-        money_machine.report()
-    else:
-        drink = menu.find_drink(choice)
-        is_enough_ingredients = coffee_maker.is_resource_sufficient(drink)
-        is_payment_successful = money_machine.make_payment(drink.cost)
-        if is_enough_ingredients and is_payment_successful:
-            coffee_maker.make_coffee(drink)
+# creating an empty list
+question_bank = []
+
+# looping through the question data and appending it to a new variable
+for question in question_data:
+    # creating varuables to access the data from the Question class
+    question_text = question["question"]
+    question_answer = question["correct_answer"]
+    # gathering the data 
+    new_question = Question(question_text, question_answer)
+    # creating a new question from the question data
+    question_bank.append(new_question)
+# iterating throught the question data and fetching those datas as per our requirement
+# print(question_bank[0].text, question_bank[0].answer)
+quiz = QuizBrain(question_bank)
+
+quiz.next_question()
+
+while quiz.still_has_questions:
+    quiz.next_question()
+
+
+
+print("You've completed the quiz")
+print(f"Your final score was: {quiz.score}/{quiz.question_number}")
