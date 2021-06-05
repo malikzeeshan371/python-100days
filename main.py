fd222de41b27c12b5e243b09c0230311890494cd
Sumon Chhetry <53363420+sumonchhetry@users.noreply.github.com>
2021-06-06 01:42:15 +0530

    Add files via upload
    #Day 18 project
    #the turtle racing game with python

--- main.py
+++ main.py
@@ -1,68 +1,68 @@
-# import colorgram
-#
-# rgb_colors = []
-# colors = colorgram.extract('image.jpg', 30)
-# for color in colors:
-#     r = color.rgb.r
-#     g = color.rgb.g
-#     b = color.rgb.b
-#     new_color = (r, g, b)
-#     rgb_colors.append(new_color)
-#
-# print(rgb_colors)
-import turtle as turtle_module
+from turtle import Turtle, Screen
 import random
+is_race_on = False
+screen = Screen()
+# setting up the screen height and width
+screen.setup(width=500, height=400)
 
-turtle_module.colormode(255)
-tim = turtle_module.Turtle()
-tim.speed("fastest")
-tim.penup()
-tim.hideturtle()
-color_list = [
-                (26, 108, 164),
-                (194, 38, 81),
-                (237, 161, 50),
-                (234, 215, 86),
-                (226, 237, 229),
-                (223, 137, 176),
-                (143, 108, 57),
-                (102, 197, 219),
-                (205, 166, 30),
-                (21, 57, 132),
-                (214, 74, 91),
-                (238, 89, 49),
-                (119, 192, 140),
-                (142, 208, 227),
-                (4, 185, 176),
-                (106, 108, 198),
-                (138, 29, 72),
-                (5, 161, 86),
-                (98, 51, 36),
-                (23, 156, 209),
-                (229, 167, 185),
-                (173, 185, 222),
-                (29, 90, 95),
-                (233, 172, 162),
-                (155, 212, 190),
-                (88, 46, 33),
-                (244, 207, 5)
-            ]
+# taking the bet at the beggining of the race
+user_bet = screen.textinput(title="Make your bet", prompt="which turtle will win the race")
+# print(user_bet)
+colors = ["red", "green", "blue", "yellow", "purple", "orange"]
+y_position = [-70, -40, -10, 20, 50, 80]
+all_turtles = []
 
-tim.setheading(225)
-tim.forward(300)
-tim.setheading(0)
-number_of_dots = 100
-
-for dot_count in range(1, number_of_dots + 1):
-    tim.dot(20, random.choice(color_list))
-    tim.forward(50)
-    if dot_count % 10 == 0:
-        tim.setheading(90)
-        tim.forward(50)
-        tim.setheading(180)
-        tim.forward(500)
-        tim.setheading(0)
+# to create different turtle at different coordinance with different color
+for turtle_index in range(0, 5):
+    new_turtle = Turtle(shape="turtle")
+    new_turtle.speed(1)
+    new_turtle.color(colors[turtle_index])
+    new_turtle.penup()
+    new_turtle.goto(x=-235, y=y_position[turtle_index])
+    all_turtles.append(new_turtle)
+# starting the race
+if user_bet:
+    is_race_on = True
+# creating the loop to run the race
+while is_race_on:
+    # ending the race
+    for turtle in all_turtles:
+        if turtle.xcor() > 230:
+            is_race_on = False
+            # selecting the winner
+            winning_color = turtle.pencolor()
+            if winning_color == user_bet:
+                print(f"You've WON! The {winning_color} turtle is the winner!")
+            else:
+                print(f"You've LOST! The {winning_color} turtle is the winner!")
+        # creating the random movement for different turtles
+        rand_distance = random.randint(0, 10)
+        turtle.forward(rand_distance)
 
 
-screen = turtle_module.Screen()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 screen.exitonclick()
