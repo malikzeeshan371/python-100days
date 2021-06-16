02ac73bb436985b59b7e9cdc3a71c94771890170
Sumon Chhetry <53363420+sumonchhetry@users.noreply.github.com>
2021-06-16 14:14:49 +0530

    Add files via upload
    Random Password Generator with GUI

--- main.py
+++ main.py
@@ -1,90 +1,83 @@
 from tkinter import *
-import math
-# Constants #
-PINK = "#e2979c"
-RED = "#e7305b"
-GREEN = "#9bdeac"
-YELLOW = "#f7f5dd"
-FONT_NAME = "Courier"
-WORK_MIN = 25
-SHORT_BREAK_MIN = 5
-LONG_BREAK_MIN = 15
-reps = 0
-timer = None
+from tkinter import messagebox
+from random import choice, randint, shuffle
+import pyperclip
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 
-def reset_timer():
-    window.after_cancel(timer)
-    canvas.itemconfig(timer_text, text="00:00")
-    title_label.config(text="Timer", fg=GREEN)
-    check_marks.config(text="")
-    global reps
-    reps = 0
+# Password Generator Project
+def generate_password():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
+    # nr_letters = random.randint(8, 10)
+    # nr_symbols = random.randint(2, 4)
+    # nr_numbers = random.randint(2, 4)
 
-def start_timer():
-    global reps
-    reps += 1
+    password_letters = [choice(letters) for _ in range(randint(8, 18))]
+    password_symbols = [choice(symbols) for _ in range(randint(2, 4))]
+    password_numbers = [choice(numbers) for _ in range(randint(2, 4))]
 
-    work_sec = WORK_MIN * 60
-    short_break_sec = SHORT_BREAK_MIN * 60
-    long_break_sec = LONG_BREAK_MIN * 60
-    count_down(5 * 60)
+    password_list = password_letters + password_symbols + password_numbers
 
-    if reps % 8 == 0:
-        count_down(long_break_sec)
-        title_label.config(text="Break", fg=RED)
-    elif reps % 2 == 0:
-        count_down(short_break_sec)
-        title_label.config(text="Break", fg=PINK)
+    shuffle(password_list)
+
+    # password = ""
+    # for char in password_list:
+    #   password += char
+    password = "".join(password_list)
+    password_entry.insert(0, password)
+    pyperclip.copy(password)
+    # print(f"Your password is: {password}")
+# ---------------------------- SAVE PASSWORD ------------------------------- #
+def save_password():
+    website = website_entry.get()
+    email = email_entry.get()
+    password = password_entry.get()
+
+    if len(website) == 0 or len(password) == 0:
+        messagebox.showinfo(title="Oops", message="Please make sure not to left any input field empty")
     else:
-        count_down(work_sec)
-        title_label.config(text="Work", fg=GREEN)
+        is_okay = messagebox.askokcancel(title=website, message=f"There are the details entered: \nEmail: {email}"
+                                                            f"\nPassword: {password} \nIs it okay to save ?")
+        if is_okay:
+            with open("data.txt", "a") as data_file:
+                data_file.write(f"{website} | {email} | {password}\n")
+                website_entry.delete(0, END)
+                password_entry.delete(0, END)
 
 
-def count_down(count):
-    count_min = math.floor(count / 60)
-    count_sec = (count % 60)
-    if count_sec == 0:
-        count_sec = "00"
-    elif count_sec < 10:
-        count_sec = f"0{count_sec}"
-
-    canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
-    if count > 0:
-        global timer
-        timer = window.after(1000, count_down, count - 1)
-    else:
-        start_timer()
-        marks = ""
-        work_sessions = math.floor(reps/2)
-        for _ in range(work_sessions):
-            marks += "✓"
-        check_marks.config(text=marks)
-
-
+# ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
-window.title("Pomodoro")
-window.config(padx=100, pady=50, bg=YELLOW)
+window.title("Password Manager")
+window.config(padx=50, pady=50)
 
+canvas = Canvas(width=200, height=200)
+lock_pic = PhotoImage(file="logo.png")
+canvas.create_image(100, 100, image=lock_pic)
+canvas.grid(column=1, row=0)
 
-title_label = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 50))
-title_label.grid(column=1, row=0)
-
-
-start_button = Button(text="Start", bg=YELLOW, highlightthickness=0, command=start_timer)
-start_button.grid(column=0, row=2)
-reset_button = Button(text="Reset", bg=YELLOW, highlightthickness=0, command=reset_timer)
-reset_button.grid(column=2, row=2)
-
-check_marks = Label(fg=GREEN, bg=YELLOW, font=(FONT_NAME, 25))
-check_marks.grid(column=1, row=3)
-
-canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
-tomato_pic = PhotoImage(file="tomato.png")
-canvas.create_image(100, 112, image=tomato_pic)
-timer_text = canvas.create_text(105, 133, text="00:00", fill="white", font=(FONT_NAME, 28, "bold"))
-canvas.grid(column=1, row=1)
-
+# labels
+website_label = Label(text="Website:")
+website_label.grid(column=0, row=1)
+email_label = Label(text="Email/Username:")
+email_label.grid(column=0, row=2)
+password_label = Label(text="Password:")
+password_label.grid(column=0, row=3)
+# entry
+website_entry = Entry(width=35)
+website_entry.grid(column=1, row=1, columnspan=2, sticky=EW)
+website_entry.focus()
+email_entry = Entry(width=35)
+email_entry.grid(column=1, row=2, columnspan=2, sticky=EW)
+email_entry.insert(0, "sumonchhetry@email.com")
+password_entry = Entry(width=21)
+password_entry.grid(column=1, row=3, sticky=EW)
+# Buttons
+pw_button = Button(text="Generate Password", highlightthickness=0, command=generate_password)
+pw_button.grid(column=2, row=3)
+add_button = Button(text="Add", width=36,  highlightthickness=0, command=save_password)
+add_button.grid(column=1, row=4, columnspan=2, sticky=EW)
 
 window.mainloop()
