dc86d7c80fbc2d6f35e02271a46b25211de1c711
Sumon Chhetry <53363420+sumonchhetry@users.noreply.github.com>
2021-06-16 10:21:26 +0530

    Add files via upload
    Pomodoro timer

--- main.py
+++ main.py
@@ -1,13 +1,90 @@
-import pandas
+from tkinter import *
+import math
+# Constants #
+PINK = "#e2979c"
+RED = "#e7305b"
+GREEN = "#9bdeac"
+YELLOW = "#f7f5dd"
+FONT_NAME = "Courier"
+WORK_MIN = 25
+SHORT_BREAK_MIN = 5
+LONG_BREAK_MIN = 15
+reps = 0
+timer = None
 
-phonetic_data = pandas.read_csv("nato_phonetic_alphabet.csv")
-# Create a dictionary in this format:
-# {"A": "Alfa", "B": "Bravo"}
-# using dict comprehension method
-phonetic_dict = {row.letter: row.code for (index, row) in phonetic_data.iterrows()}
-# print(phonetic_dict)
 
-# Create a list of the phonetic code words from a word that the user inputs.
-word = input("Enter a word:").upper()
-output_list = [phonetic_dict[letter] for letter in word]
-print(output_list)
+def reset_timer():
+    window.after_cancel(timer)
+    canvas.itemconfig(timer_text, text="00:00")
+    title_label.config(text="Timer", fg=GREEN)
+    check_marks.config(text="")
+    global reps
+    reps = 0
+
+
+def start_timer():
+    global reps
+    reps += 1
+
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
+    count_down(5 * 60)
+
+    if reps % 8 == 0:
+        count_down(long_break_sec)
+        title_label.config(text="Break", fg=RED)
+    elif reps % 2 == 0:
+        count_down(short_break_sec)
+        title_label.config(text="Break", fg=PINK)
+    else:
+        count_down(work_sec)
+        title_label.config(text="Work", fg=GREEN)
+
+
+def count_down(count):
+    count_min = math.floor(count / 60)
+    count_sec = (count % 60)
+    if count_sec == 0:
+        count_sec = "00"
+    elif count_sec < 10:
+        count_sec = f"0{count_sec}"
+
+    canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
+    if count > 0:
+        global timer
+        timer = window.after(1000, count_down, count - 1)
+    else:
+        start_timer()
+        marks = ""
+        work_sessions = math.floor(reps/2)
+        for _ in range(work_sessions):
+            marks += "✓"
+        check_marks.config(text=marks)
+
+
+window = Tk()
+window.title("Pomodoro")
+window.config(padx=100, pady=50, bg=YELLOW)
+
+
+title_label = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 50))
+title_label.grid(column=1, row=0)
+
+
+start_button = Button(text="Start", bg=YELLOW, highlightthickness=0, command=start_timer)
+start_button.grid(column=0, row=2)
+reset_button = Button(text="Reset", bg=YELLOW, highlightthickness=0, command=reset_timer)
+reset_button.grid(column=2, row=2)
+
+check_marks = Label(fg=GREEN, bg=YELLOW, font=(FONT_NAME, 25))
+check_marks.grid(column=1, row=3)
+
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
+tomato_pic = PhotoImage(file="tomato.png")
+canvas.create_image(100, 112, image=tomato_pic)
+timer_text = canvas.create_text(105, 133, text="00:00", fill="white", font=(FONT_NAME, 28, "bold"))
+canvas.grid(column=1, row=1)
+
+
+window.mainloop()
