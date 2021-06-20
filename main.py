4f5094b03c55d38cdc6335fe51f35a9779464185
Sumon Chhetry <53363420+sumonchhetry@users.noreply.github.com>
2021-06-21 01:23:15 +0530

    Add files via upload
    a programm to check the iss postion and check if it's in my lat and long

--- main.py
+++ main.py
@@ -1,83 +1,56 @@
-from tkinter import *
-from tkinter import messagebox
-from random import choice, randint, shuffle
-import pyperclip
-# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+import smtplib
+import requests
+from datetime import datetime
+import time
+
+# Your latitude
+MY_LAT = 22.572645
+# Your longitude
+MY_LONG = 88.363892
+
+MY_EMAIL = "sumonchhetry01@gmail.com"
+MY_PASSWORD = "abcdefgh@0987"
+
+def iss_overhead():
+    response = requests.get(url="http://api.open-notify.org/iss-now.json")
+    response.raise_for_status()
+    data = response.json()
+
+    iss_latitude = float(data["iss_position"]["latitude"])
+    iss_longitude = float(data["iss_position"]["longitude"])
+
+    #Your position is within +5 or -5 degrees of the ISS position.
+    if MY_LAT-5 <= iss_latitude <= MY_LAT+5 and MY_LONG-5 <= iss_longitude <= MY_LONG+5:
+        return True
 
 
-# Password Generator Project
-def generate_password():
-    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+def is_night():
+    parameters = {
+        "lat": MY_LAT,
+        "lng": MY_LONG,
+        "formatted": 0,
+    }
 
-    # nr_letters = random.randint(8, 10)
-    # nr_symbols = random.randint(2, 4)
-    # nr_numbers = random.randint(2, 4)
+    response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+    response.raise_for_status()
+    data = response.json()
+    sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+    sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
 
-    password_letters = [choice(letters) for _ in range(randint(8, 18))]
-    password_symbols = [choice(symbols) for _ in range(randint(2, 4))]
-    password_numbers = [choice(numbers) for _ in range(randint(2, 4))]
+    time_now = datetime.now().hour
 
-    password_list = password_letters + password_symbols + password_numbers
+    if time_now >= sunset or time_now <= sunrise:
+        return True
 
-    shuffle(password_list)
+while True:
+    time.sleep(60)
+    if iss_overhead() and is_night():
+        connection = smtplib.SMTP("smtp.gmail.com")
+        connection.starttls()
+        connection.login(MY_EMAIL, MY_PASSWORD)
+        connection.sendmail(
+            from_addr=MY_EMAIL,
+            to_addrs=MY_EMAIL,
+            msg="Subject: Look up\n\nThe ISS is above you in the sky."
+        )
 
-    # password = ""
-    # for char in password_list:
-    #   password += char
-    password = "".join(password_list)
-    password_entry.insert(0, password)
-    pyperclip.copy(password)
-    # print(f"Your password is: {password}")
-# ---------------------------- SAVE PASSWORD ------------------------------- #
-def save_password():
-    website = website_entry.get()
-    email = email_entry.get()
-    password = password_entry.get()
-
-    if len(website) == 0 or len(password) == 0:
-        messagebox.showinfo(title="Oops", message="Please make sure not to left any input field empty")
-    else:
-        is_okay = messagebox.askokcancel(title=website, message=f"There are the details entered: \nEmail: {email}"
-                                                            f"\nPassword: {password} \nIs it okay to save ?")
-        if is_okay:
-            with open("data.txt", "a") as data_file:
-                data_file.write(f"{website} | {email} | {password}\n")
-                website_entry.delete(0, END)
-                password_entry.delete(0, END)
-
-
-# ---------------------------- UI SETUP ------------------------------- #
-window = Tk()
-window.title("Password Manager")
-window.config(padx=50, pady=50)
-
-canvas = Canvas(width=200, height=200)
-lock_pic = PhotoImage(file="logo.png")
-canvas.create_image(100, 100, image=lock_pic)
-canvas.grid(column=1, row=0)
-
-# labels
-website_label = Label(text="Website:")
-website_label.grid(column=0, row=1)
-email_label = Label(text="Email/Username:")
-email_label.grid(column=0, row=2)
-password_label = Label(text="Password:")
-password_label.grid(column=0, row=3)
-# entry
-website_entry = Entry(width=35)
-website_entry.grid(column=1, row=1, columnspan=2, sticky=EW)
-website_entry.focus()
-email_entry = Entry(width=35)
-email_entry.grid(column=1, row=2, columnspan=2, sticky=EW)
-email_entry.insert(0, "sumonchhetry@email.com")
-password_entry = Entry(width=21)
-password_entry.grid(column=1, row=3, sticky=EW)
-# Buttons
-pw_button = Button(text="Generate Password", highlightthickness=0, command=generate_password)
-pw_button.grid(column=2, row=3)
-add_button = Button(text="Add", width=36,  highlightthickness=0, command=save_password)
-add_button.grid(column=1, row=4, columnspan=2, sticky=EW)
-
-window.mainloop()
